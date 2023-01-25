4f2a9a1b81203834a4bf0a43a0de5a62ad4a9f27
Emily Johnson <ejjohnso@amazon.com>
2023-01-25 15:05:40 -0800

    add users to app

--- authenticated_qs_app.py
+++ authenticated_qs_app.py
@@ -6,27 +6,18 @@ from botocore.exceptions import ClientError
 k_REGION = "us-west-2"
 k_ACCOUNT_ID = "110561467685"
 k_DASHBOARD_ID = "87f65650-a75c-427d-8d89-6d46384030f8"
-k_USER_ARN = "arn:aws:iam::110561467685:user/sm-quicklit-registered"
 k_ROLE_ARN = "arn:aws:iam::110561467685:role/Supermarine-Quicklit-GenerateEmbedUrlForRegisteredUser"
 k_OPEN_ID_TOKEN = ""
 k_DOMAINS = ["https://*.streamlit.app"]
 k_NAMESPACE = "default"
 
-# Function to generate embedded URL
-# regionId: AWS Region ID  
-# accountId: AWS account ID
-# dashboardId: Dashboard ID to embed
-# userArn: arn of registered user
-# allowedDomains: Runtime allowed domain for embedding
-# openIdToken: Token to assume role with roleArn
-# roleArn: IAM user role to use for embedding
-def generateEmbeddingURLForRegisteredUser(regionId, accountId, dashboardId, userArn, allowedDomains, openIdToken, roleArn, qs_namespace):
+def create_qs_client(region_id, role_arn):
     # Create STS client
     sts = boto.client('sts')
 
     # Assume proper role
     assumed_role = sts.assume_role(
-        RoleArn = roleArn,
+        RoleArn = role_arn,
         #WebIdentityToken = openIdToken,
         RoleSessionName = "AssumeRole_RegisteredUser"
     )
@@ -37,36 +28,65 @@ def generateEmbeddingURLForRegisteredUser(regionId, accountId, dashboardId, user
     # Create quicksight client
     qs_client = boto.client(
         'quicksight',
-        region_name = regionId,
+        region_name = region_id,
         aws_access_key_id = creds['AccessKeyId'],
         aws_secret_access_key = creds['SecretAccessKey'],
         aws_session_token = creds['SessionToken']
     )
 
+    return qs_client
+
+def register_user(qs_client, account_id, qs_namespace):
+
+    try:
+        response = qs_client.register_user(
+            AwsAccountId = account_id,
+            Namespace = qs_namespace,
+            Email = user_email,
+            IdentityType = "QUICKSIGHT",
+            UserRole = "READER"
+        )
+
+        return response
+
+    except ClientError as e:
+        st.write(e)
+        return {}   
+
+
+def list_users(qs_client, account_id, qs_namespace):
+
+    # Note: This does not properly paginate past 100.
     try:
         users = qs_client.list_users(
-            AwsAccountId = accountId,
-            MaxResults = 10,
+            AwsAccountId = account_id,
+            MaxResults = 100,
             Namespace = qs_namespace
         )
 
         user_list = users["UserList"]
+
+        # debug
         st.write(user_list)
+
+        return user_list
     
     except ClientError as e:
         st.write(e)
         return {}
 
+def generate_embedding_url_for_registered_user(qs_client, account_id, dashboard_id, user_arn, allowed_domains):
+
     try:
         response = qs_client.generate_embed_url_for_registered_user(
-            AwsAccountId = accountId,
+            AwsAccountId = account_id,
             ExperienceConfiguration = {
                 'Dashboard': {
-                    'InitialDashboardId': dashboardId
+                    'InitialDashboardId': dashboard_id
                 }
             },
-            UserArn = userArn,
-            AllowedDomains = allowedDomains,
+            UserArn = user_arn,
+            AllowedDomains = allowed_domains,
             SessionLifetimeInMinutes = 600
         )
             
@@ -76,11 +96,36 @@ def generateEmbeddingURLForRegisteredUser(regionId, accountId, dashboardId, user
         st.write(e)
         return {}
 
-response = generateEmbeddingURLForRegisteredUser(k_REGION, k_ACCOUNT_ID, k_DASHBOARD_ID, k_USER_ARN, k_DOMAINS, k_OPEN_ID_TOKEN, k_ROLE_ARN, k_NAMESPACE)
+st.title("Registered Users - QuickSight App")
+
+st.write("Please enter your email address to get started")
+
+user_email = st.text_input("Email Address", value="example@domain.com")
+
+qs_client = create_qs_client(k_REGION, k_ROLE_ARN)
+
+already_registered_users = list_users(qs_client, k_ACCOUNT_ID, k_NAMESPACE)
+
+user_arn = ""
+for i, user in enumerate(already_registered_users):
+    if user['Email'] == user_email:
+        user_arn = user['Arn'] # this is probably bad and not secure. 
+        break
+
+if user_arn == "":
+    new_user_response = register_user()
+
+    register_url = new_user_response['UserInvitationUrl']
+    st.components.v1.iframe(register_url, width=None, height=1000, scrolling=True)
+
+    user_arn = new_user_response['User']['Arn']
+
+url_response = generate_embedding_url_for_registered_user(qs_client, k_ACCOUNT_ID, k_DASHBOARD_ID, user_arn, k_DOMAINS)
 
 # render dashboard
-if "EmbedUrl" in response:
-    html = response["EmbedUrl"]
+k_EMBED_KEY = "EmbedUrl"
+if k_EMBED_KEY in url_response:
+    html = url_response[k_EMBED_KEY]
     st.components.v1.iframe(html, width=None, height=1000, scrolling=True)
 else: 
     st.write("No Embedded URL found")
